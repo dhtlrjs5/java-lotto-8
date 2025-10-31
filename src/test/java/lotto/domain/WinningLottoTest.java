7a389279ed8b2c134a2cd5cf14c1f464752605d0
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-31 13:09:07 +0900

    test: 구매 로또와 당첨 로또 비교 테스트 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
+import lotto.utils.Rank;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -50,5 +53,31 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest
+    // 당첨 번호: [1, 2, 3, 4, 5, 6], 보너스 번호: 7
+    @CsvSource(value = {
+            "1,2,3,4,5,6, FIRST",    // 6개 일치
+            "1,2,3,4,5,7, SECOND",   // 5개 일치, 보너스 일치
+            "1,2,3,4,5,10, THIRD",   // 5개 일치, 보너스 불일치
+            "1,2,3,4,8,9, FOURTH",   // 4개 일치
+            "1,2,3,8,9,10, FIFTH",   // 3개 일치
+            "1,2,8,9,10,11, MISS"    // 2개 일치
+    })
+    @DisplayName("구매 로또와 당첨 로또를 비교하여 올바른 Rank 반환")
+    void matchLottoTest(int n1, int n2, int n3, int n4, int n5, int n6, Rank expectedRank) {
+        //given (당첨 번호: [1, 2, 3, 4, 5, 6] / 보너스 번호: 7)
+        Lotto winningLottoNumbers = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, 7);
+
+        List<Integer> purchasedNumbers = List.of(n1, n2, n3, n4, n5, n6);
+        Lotto purchasedLotto = new Lotto(purchasedNumbers);
+
+        //when
+        Rank actualRank = winningLotto.match(purchasedLotto);
+
+        //then
+        assertThat(actualRank).isEqualTo(expectedRank);
     }
 }
