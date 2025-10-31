7aaacceb022b07e4bd52b53f6069cd25753b14d6
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-31 12:45:27 +0900

    fix: 불변 리스트 정렬시 발생하는 예외 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,13 +2,14 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
 
     public Lotto generateLotto() {
 
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
         numbers.sort(Integer::compareTo);
 
         return new Lotto(numbers);
